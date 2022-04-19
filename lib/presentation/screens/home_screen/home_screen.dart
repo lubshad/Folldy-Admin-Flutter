203c75e2b16d385f20fb0dc753f6959063deae57
lubshad <lubshad4u4@gmail.com>
2022-04-19 14:17:51 +0530

    core fix

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -41,7 +41,7 @@ class Home extends StatelessWidget {
                                   backgroundImage: CachedNetworkImageProvider(
                                       "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
                                 ),
-                                title: Text("Lubshad"),
+                                title: Text("Username"),
                                 // subtitle: Text("Lead Developer"),
                               ),
                             ],
