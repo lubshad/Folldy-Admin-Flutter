becb7745915023457fd88f4c0e3db2ff084b8abd
lubshad <lubshad4u4@gmail.com>
2022-06-13 13:35:30 +0530

    api constants fix

--- lib/presentation/screens/presentations_listing/presentations_listing.dart
+++ lib/presentation/screens/presentations_listing/presentations_listing.dart
@@ -1,12 +1,12 @@
 import 'package:basic_template/basic_template.dart';
 import 'package:flutter/material.dart';
-import 'package:folldy_utils/data/core/api_constants.dart';
 import 'package:folldy_admin/presentation/screens/presentations_listing/presentation_listing_controller.dart';
 import 'package:folldy_admin/presentation/theme/theme.dart';
 import 'package:folldy_admin/utils/presentation_mode.dart';
 import 'package:folldy_admin/utils/url_launcher_utils.dart';
 import 'package:folldy_utils/data/models/area_list_response.dart';
 import 'package:folldy_utils/data/models/presentation_list_response.dart';
+import 'package:folldy_admin/data/core/api_constants.dart';
 
 // ignore: must_be_immutable
 class PresentationViewArguments extends Equatable {
