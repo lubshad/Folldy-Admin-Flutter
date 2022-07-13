6884117aed871de406b2e8807c890bb6821202df
lubshad <lubshad4u4@gmail.com>
2022-07-13 17:53:53 +0530

    android version fix

--- lib/presentation/screens/home_screen/home_controller.dart
+++ lib/presentation/screens/home_screen/home_controller.dart
@@ -9,7 +9,7 @@ import '../area_vise_presentation_listing/area_vise_presentation_listing.dart';
 import '../universities_listing/universities_listing.dart';
 
 class HomeController extends ChangeNotifier {
-  DrawerItem selectedItem = DrawerItem.appSettings;
+  DrawerItem selectedItem = DrawerItem.dashboard;
 
   void selectItem(DrawerItem item) {
     selectedItem = item;
